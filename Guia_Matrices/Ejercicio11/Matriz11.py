1ca4916700d7d2e75e1f9cceb20aa4b007a02889
FacunditoEsqui <facundo.esquivelet32@gmail.com>
2025-07-18 11:08:38 -0300

    Update Matriz11.py

--- Guia_Matrices/Ejercicio11/Matriz11.py
+++ Guia_Matrices/Ejercicio11/Matriz11.py
@@ -1,31 +1,31 @@
 Matriz = [
-[0, 4, -2],
-[5, 0, -3],
-[0, 2, 0]
+[4, 5, -4],
+[5, 2, -1],
+[1, 0, 7]
 ]
 
 columnas = 3
 
-Fila1 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+Fila1 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 while Fila1 > 2:
-    print ("HACE CASO")
-    Fila1 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+    print ("elegi del 0 al 2")
+    Fila1 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 while Fila1 < 0:
-    print ("HACE CASO")
-    Fila1 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+    print ("elegi del 0 al 2")
+    Fila1 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 
-Fila2 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+Fila2 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 while Fila2 > 2:
-    print ("HACE CASO")
-    Fila2 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+    print ("elegi del 0 al 2")
+    Fila2 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 while Fila2 < 0:
-    print ("HACE CASO")
-    Fila2 = int(input("Selecciona una fila de la matriz (del 0 al 2) para intercambiar: "))
+    print ("elegi del 0 al 2")
+    Fila2 = int(input("Elegi fila de la matriz (del 0 al 2) para intercambiar: "))
 
 
 q = 0
@@ -36,23 +36,23 @@ for i in range(columnas):
         if i == Fila1:
             p = p +1
             if p == 1:
-                numa1 = Matriz[i][j]
+                numA1 = Matriz[i][j]
             if p == 2:
-                numa2 = Matriz[i][j]
+                numA2 = Matriz[i][j]
             if p == 3:
-                numa3 = Matriz[i][j]
+                numA3 = Matriz[i][j]
         
         if i == Fila2:
             q = q +1
             if q == 1:
-                numb1 = Matriz[i][j]
+                numB1 = Matriz[i][j]
             if q == 2:
-                numb2 = Matriz[i][j]
+                numB2 = Matriz[i][j]
             if q == 3:
-                numb3 = Matriz[i][j]
+                numB3 = Matriz[i][j]
 
-Matriz[Fila2] = [numa1, numa2, numa3]
+Matriz[Fila2] = [numA1, numA2, numA3]
 
-Matriz[Fila1] = [numb1, numb2, numb3]
+Matriz[Fila1] = [numB1, numB2, numB3]
 
 print("Asi quedo la matriz: ", Matriz)
