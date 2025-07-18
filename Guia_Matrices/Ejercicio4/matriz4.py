048825d28e1057d205f2bc6972fb9a3d3360e400
FacunditoEsqui <facundo.esquivelet32@gmail.com>
2025-07-18 11:19:36 -0300

    Update matriz4.py

--- Guia_Matrices/Ejercicio4/matriz4.py
+++ Guia_Matrices/Ejercicio4/matriz4.py
@@ -1,26 +1,26 @@
 Matriz = [
-[0, 4, -2],
-[5, 0, -3],
-[0, 2, 0]
+[4, 8, 3],
+[9, 7, 5],
+[8, 1, 4]
 ]
 
 columnas = 3
 numeroMayor = -99999
 
-columna = int(input("Selecciona una columna de la matriz (del 0 al 2): "))
+columna = int(input("Elegi una columna de la matriz (del 0 al 2): "))
 
 while columna > 2:
-    print ("HACE CASO")
-    columna = int(input("Selecciona una columna de la matriz (del 0 al 2): "))
+    print ("elegi del 0 al 2")
+    columna = int(input("Elegi una columna de la matriz (del 0 al 2): "))
 
 while columna < 0:
-    print ("HACE CASO")
-    columna = int(input("Selecciona una columna de la matriz (del 0 al 2): "))
+    print ("elegi del 0 al 2")
+    columna = int(input("Elegi columna de la matriz (del 0 al 2): "))
 
 for i in range(columnas):
     for j in range(columnas): 
         if j == columna:
-            if Matriz[i][j] > numeroMayor:
-                numeroMayor = Matriz[i][j]
+            if Matriz[i][j] > Mayor:
+                Mayor = Matriz[i][j]
 
-print(numeroMayor)
+print(Mayor)
