c964c6c7fabdcf2a02c0eb7d3ed8b4261c2f4c43
FacunditoEsqui <facundo.esquivelet32@gmail.com>
2025-07-18 11:21:16 -0300

    Update Matriz7.py

--- Guia_Matrices/Ejercicio7/Matriz7.py
+++ Guia_Matrices/Ejercicio7/Matriz7.py
@@ -1,16 +1,16 @@
 Matriz = [
-[0, 4, -2],
-[5, 0, -3],
-[0, 2, 0]
+[4, 8, 3],
+[9, 7, 5],
+[8, 1, 4]
 ]
 
 columnas = 3
-numeroMayor = -99999
+Mayor = -99999
 
 for i in range(columnas):
     for j in range(columnas): 
-        if Matriz[i][j] > numeroMayor:
-            numeroMayor = Matriz[i][j]
+        if Matriz[i][j] > Mayor:
+            Mayor = Matriz[i][j]
             posicion = i,j
 
 print(posicion)
